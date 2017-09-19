96e297d605a24c6bdc414b91f95983447525246c
Rik Meijer <ha.meijer@avans.nl>
2017-09-19 12:14:13 +0200

    logout user

--- public/gatekeeper.php
+++ public/gatekeeper.php
@@ -3,6 +3,9 @@
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Avans.php';
 
+/**
+ * @var $server \Avans\NamenLeren\Avans
+ */
 $server = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'sso.php';
 
 session_start();
@@ -14,20 +17,26 @@ if (array_key_exists('token_credentials', $_SESSION)) {
     }
     $tokenCredentials = unserialize($_SESSION['token_credentials']);
     $user = $server->getUserDetails($tokenCredentials);
-
     $protected_location = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'protected';
     if ($_GET['_p'] === '') {
         include $protected_location . DIRECTORY_SEPARATOR . 'index.html';
-    } else {
-        $path = str_replace(['../','./'], '', $_GET['_p']);
-        $filename = $protected_location . DIRECTORY_SEPARATOR . $path;
-        if (!file_exists($filename)) {
-            http_response_code(404);
-            exit('File not found');
-        }
-        header('Content-Type: ' . mime_content_type($filename));
-        include $filename;
+        exit;
+    } elseif ($_GET['_p'] === 'logout') {
+        unset($_SESSION['token_credentials']);
+        header("Location: /");
+        exit;
     }
+
+    $path = str_replace(['../','./'], '', $_GET['_p']);
+    $filename = $protected_location . DIRECTORY_SEPARATOR . $path;
+    if (!file_exists($filename)) {
+        http_response_code(404);
+        exit('File not found');
+    }
+    header('Content-Type: ' . mime_content_type($filename));
+    include $filename;
+    exit;
+
 } elseif (isset($_GET['oauth_token']) && isset($_GET['oauth_verifier'])) {
     $temporaryCredentials = unserialize($_SESSION['temporary_credentials']);
     $tokenCredentials = $server->getTokenCredentials($temporaryCredentials, $_GET['oauth_token'], $_GET['oauth_verifier']);
@@ -35,11 +44,15 @@ if (array_key_exists('token_credentials', $_SESSION)) {
     $_SESSION['token_credentials'] = serialize($tokenCredentials);
     session_write_close();
     header("Location: /");
+    exit;
+
 } elseif (isset($_GET['denied'])) {
     echo 'Hey! You denied the client access to your Avans account! If you did this by mistake, you should <a href="/">try again</a>.';
-} else {
-    $temporaryCredentials = $server->getTemporaryCredentials();
-    $_SESSION['temporary_credentials'] = serialize($temporaryCredentials);
-    session_write_close();
-    $server->authorize($temporaryCredentials);
+    exit;
+
 }
+
+$temporaryCredentials = $server->getTemporaryCredentials();
+$_SESSION['temporary_credentials'] = serialize($temporaryCredentials);
+session_write_close();
+$server->authorize($temporaryCredentials);
